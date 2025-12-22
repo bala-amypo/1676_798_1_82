8690d88b2ffdbe92053555da59ec9c849c90327e
bala-amypo <bala@amypo.in>
2025-12-22 08:29:55 +0000

    User commit

--- src/main/java/com/example/demo/util/ProducitivityCalculator.java
+++ src/main/java/com/example/demo/util/ProducitivityCalculator.java
@@ -3,6 +3,6 @@ package com.example.demo.util;
 public class ProductivityCalculator {
 
     public static int compute(int completed, int pending, int overdue) {
-        return completed * 2 - pending - overdue * 2;
+        return completed - pending - overdue;
     }
 }
