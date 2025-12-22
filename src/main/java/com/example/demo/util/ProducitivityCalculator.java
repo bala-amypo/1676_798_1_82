5141635a0d56449e07480a8c0b22ee80b7b8aa2a
bala-amypo <bala@amypo.in>
2025-12-22 03:45:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/ProducitivityCalculator.java
@@ -0,0 +1,11 @@
+public class ProductivityCalculator {
+
+    public static double compute(double h, int t, int m) {
+        h = Math.max(0, h);
+        t = Math.max(0, t);
+        m = Math.max(0, m);
+
+        double score = (h * 10) + (t * 5) + (m * 2);
+        return Math.min(100.0, score);
+    }
+}
